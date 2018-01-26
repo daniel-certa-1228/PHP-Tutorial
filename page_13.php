11c83e8c498ef15b759110f280a52c586566605a
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-26 13:51:10 -0600

    file writing

--- page_13.php
+++ page_13.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>A Simple PHP File | Page 11</title>
+    <title>A Simple PHP File | Page 13</title>
 </head>
 <body>
     <h1><?php echo "Page 13"; ?></h1>
