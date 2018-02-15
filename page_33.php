6caa286d5abf7aeaa154409e90f0e784eba3c3c5
daniel-certa-1228 <mfdancerta@gmail.com>
2018-02-15 10:35:31 -0600

    get last inserted row

--- page_33.php
+++ page_33.php
@@ -7,8 +7,30 @@
     <title>A Simple PHP File | Page 33</title>
 </head>
 <body>
-    <h1><?php echo "Page 32"; ?></h1>
+    <h1><?php echo "Page 33"; ?></h1>
     <h2>Get the ID of Last Inserted Row</h2>
+
+    <?php
+        $connection = new mysqli("127.0.0.1", "root", "password", "tutorial_2");
+
+        if($connection === false){
+            die ("Error: Could not connect ." . mysqli_connect_error() . "<br>");
+        }  else  {
+            echo "Connected successfully. Host info: " . mysqli_get_host_info($connection) . "<br>";
+        }
+
+        $insert = "INSERT INTO persons (first_name, last_name, email) VALUES ('Neville', 'Longbottom', 'neville@mail.com');";
+
+        if(mysqli_query($connection, $insert)){
+            //obtain the last inserted id
+            $last_id = mysqli_insert_id($connection);
+            echo "Records inserted successfully. Last inserted ID is: " . $last_id;
+        }  else  {
+            echo "ERROR: Could not able to execute $insertsql. " . mysqli_error($connection);
+        }
+
+        mysqli_close($connection);        
+    ?>
     
 </body>
 </html>
